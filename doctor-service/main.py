395538092998cb615d266954976f797ffc29868d
AyanimaHettiarachchi <172464573+AyanimaHettiarachchi@users.noreply.github.com>
2026-03-30 20:40:12 +0530

    Doctor-service change new

--- doctor-service/main.py
+++ doctor-service/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Doctor Microservice", version="1.0.0")
 def read_root():
     return {"message": "Doctor Microservice is running"}
 
-@app.get("/api/doctors", response_model=list[Doctor])
+@app.get("/api/doctors", response_model=list[Doctor]) # Get all doctors
 def get_all():
     return db.get_all()
 
